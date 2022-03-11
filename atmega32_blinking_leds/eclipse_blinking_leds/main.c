dd9a9fe75d80bc9acf7969b5b0afaea91ecefc43
Sameh Nagi <eng.same7.la@gmail.com>
2022-03-11 23:17:08 +0100

    Blinking LEDs project

---
+++ atmega32_blinking_leds/eclipse_blinking_leds/main.c
@@ -0,0 +1,21 @@
+/*
+ * main.c
+ *
+ *  Created on: Mar 11, 2022
+ *      Author: Sameh Nagi
+ */
+
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main(void)
+{
+  DDRC = 0xFF; //Nakes PORTC as Output
+  while(1) //infinite loop
+  {
+    PORTC = 0xFF; //Turns ON All LEDs
+    _delay_ms(1000); //1 second delay
+    PORTC= 0x00; //Turns OFF All LEDs
+    _delay_ms(1000); //1 second delay
+  }
+}
